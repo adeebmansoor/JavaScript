4fa52dc9914cabeacf2ea3c2a18d0cafa0b84118
adeeb mansoor <adeebmansoor17@gmail.com>
2024-09-13 02:04:19 +0530

    new

---
+++ Ch3Loops.js
@@ -0,0 +1,50 @@
+// for Loop
+// Program for factorial of a n natural numbers?
+/*const prompt = require("prompt-sync")();
+let fact = 1;
+let n = prompt("Enter the value of n: ");
+n = Number.parseInt(n);
+for(let i=1; i<=n; i++){
+    fact *= i;
+}
+console.log("Factorial of " + n + " is " + fact); */
+
+// for-in Loop (it loops through the keys of an object)
+/* let obj = {
+    A : '76',
+    B : '90',
+    C : '56',
+    D : '32',
+}
+for (let a in obj){
+    console.log("Marks of " + a + " are " + obj[a]);
+} */
+
+// For-of Loop (it loops through the values of an object)
+/* for (let b of "Adeeb Mansoor"){
+    console.log(b);
+} */
+
+// while Loop
+// Program for sum of n natural numbers?
+/* const prompt = require("prompt-sync")();
+let n = prompt("Enter the value of n: ");
+n = Number.parseInt(n);
+let sum = 0;
+let i = 0;
+while(i<=n){
+    sum += i;
+    i++;
+}
+console.log(sum); */
+
+// do-while Loop
+// Multiplication table of nth number?
+const prompt = require("prompt-sync")();
+let n = prompt("Enter the value of n: ");
+n = Number.parseInt(n);
+let i = 1;
+do{
+    console.log(n + " X " + i + " = " + n*i);
+    i++;
+}while(i<=10)
---
+++ PracticeSet3.js
@@ -0,0 +1,36 @@
+// CH3 - Q1 WAP to print the marks of the students in the object using for loop?
+let marks = {
+    Harry : '98',
+    Rohan : '56',
+    Aakash : '23'
+}
+// for(i=0; i<Object.keys(marks).length; i++){
+//     console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]]);
+// }
+
+// Q2 WAP to print the marks of the students in the object using for-in loop?
+// for (let b in marks){
+//     console.log("Marks of " + b + " are " + marks[b]);
+// }
+
+// Q3 WAP to print "Try Again" until the user enters the correct number?
+// let cnum = 8;
+// const prompt = require("prompt-sync")();
+// let num;
+// while (true){
+//     num = prompt("Enter the number: ");
+//     if(num != cnum){
+//         console.log("Try Again!");
+//     }
+//     else if(num == cnum){
+//         console.log("You have entered the correct number");
+//         break;
+//     }
+// }
+
+// Q4 Write a function to find the mean of 5 numbers?
+const mean=(a,b,c,d,e)=>{
+return ((a+b+c+d+e)/5);
+}
+console.log(mean(1,2,3,4,5));
+
