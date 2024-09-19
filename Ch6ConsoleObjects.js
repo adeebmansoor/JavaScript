5fe611d4808d5967d252e6cb44a8b615899d2de6
adeeb mansoor <adeebmansoor17@gmail.com>
2024-09-20 03:02:38 +0530

    new commit

--- Ch6ConsoleObjects.js
+++ Ch6ConsoleObjects.js
@@ -1,1 +1,25 @@
-console.log(console);
+// console.log(console);
+
+// Important Console Objects:
+// 1. console.assert()    it is used to assert a condition
+// 2. console.clear()     it is used to clear the whole console
+// 3. console.log()       it is used to output a message to the console
+// 4. console.table()     displays a tabular data
+// 5. console.warn()      used for warnings
+// 6. console.errors()    used for errors
+// 7. console.info()      used for special information
+// 8. console.time()      it is used to measure the time of the process
+// 9. console.timeEnd()   it is used to measure the time of the process
+
+
+// Use of Alert, Prompt & Confirm in JavaScript
+// alert("Hello!");
+// let prompt = require("prompt-sync")();
+// let a = prompt("Enter a number: ");
+// let write = confirm("Should I write the number");
+// if(write){
+//     document.write(a);
+// }
+// else{
+//     document.write("Allow me to write");
+// }
---
+++ PracticeSet6.js
@@ -0,0 +1,48 @@
+// Ch6 Q1 - WAP using prompt function to take an input of age as a value from user and use alert to tell him if he can drive?
+// let age = prompt("Enter your age: ");
+// age = Number.parseInt(age);
+// if(age<18){
+//     alert("No you cannot drive!");
+// }
+// else if(age>18){
+//     alert("Yes you can drive!");
+// }
+
+// Q2 - In Q1 use confirm to ask if the user want to see the prompt again?
+// do{
+//     let age = prompt("Enter your age: ");
+// age = Number.parseInt(age);
+// if(age<18){
+//     alert("No you cannot drive!");
+// }
+// else if(age>=18){
+//     alert("Yes you can drive!");
+// }
+// }while(confirm("Want to see the prompt again?"));
+
+// Q3 - In Q2 use console.error if age entered is negative?
+// do{
+//     let age = prompt("Enter your age: ");
+// age = Number.parseInt(age);
+// if(age<0){
+//     console.error("Enter a valid age!");
+//     break;
+// }
+// else if(age<18){
+//     alert("No you cannot drive!");
+// }
+// else if(age>=18){
+//     alert("Yes you can drive!");
+// }
+// }while(confirm("Want to see the prompt again?"))
+
+// Q4 - WAP to change a url to google.com if the user enters a number greater than 4?
+// let num = prompt("Enter a Number: ");
+// num = Number.parseInt(num);
+// if(num > 4){
+//     location.href = "https://google.com";
+// }
+
+// Q5 - Change the background of the page as per the choice of the user?
+// Run this code in browser...
+document.body.style.background = prompt("Enter the color: ");
