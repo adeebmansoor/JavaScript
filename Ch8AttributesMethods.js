03bf9095f9907509735439a57f8401a9719c5917
adeeb mansoor <adeebmansoor17@gmail.com>
2024-10-02 01:39:14 +0530

    new

---
+++ Ch8AttributesMethods.js
@@ -0,0 +1,12 @@
+let first = document.getElementById("first");
+
+// elem.getAttribute("name") - used to get the value of an attribute.
+console.log(first.getAttribute("class"));
+
+// elem.hasAttribute("name") - used to check for existence of an attribute.
+console.log(first.hasAttribute("class"));
+console.log(first.hasAttribute("style"));
+
+// elem.setAttribute("name" , "value") - used to set the value of an attribute.
+// first.setAttribute("hidden","true");
+first.setAttribute("class","true sahiHai");
