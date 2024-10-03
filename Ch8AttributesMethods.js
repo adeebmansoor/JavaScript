a327229d5618756c2740aca2e22a18d83c99a7ff
adeeb mansoor <adeebmansoor17@gmail.com>
2024-10-04 03:27:27 +0530

    new

--- Ch8AttributesMethods.js
+++ Ch8AttributesMethods.js
@@ -1,12 +1,23 @@
-let first = document.getElementById("first");
+let firstElem = document.getElementById("first");
 
 // elem.getAttribute("name") - used to get the value of an attribute.
-console.log(first.getAttribute("class"));
+console.log(firstElem.getAttribute("class"));
 
 // elem.hasAttribute("name") - used to check for existence of an attribute.
-console.log(first.hasAttribute("class"));
-console.log(first.hasAttribute("style"));
+console.log(firstElem.hasAttribute("class"));
+console.log(firstElem.hasAttribute("style"));
 
 // elem.setAttribute("name" , "value") - used to set the value of an attribute.
-// first.setAttribute("hidden","true");
-first.setAttribute("class","true sahiHai");
+// firstElem.setAttribute("hidden","true");
+// firstElem.setAttribute("class","true sahiHai");
+
+// elem.removeAttribute("name") - used to remove the attribute from an element.
+// firstElem.removeAttribute("class");
+
+// elem.attributes() - used to get the collection of all attributes.
+console.log(firstElem.attributes);
+
+// Creating custom attributes starting with 'data-name' :
+console.log(firstElem.dataset);
+console.log(firstElem.dataset.game);
+console.log(firstElem.dataset.player);
