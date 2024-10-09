a4463afa9cd714c84640e75b1ee66680fd7a0b86
adeeb mansoor <adeebmansoor17@gmail.com>
2024-10-10 03:38:17 +0530

    new

---
+++ Ch8InsertAdjacentHTML.js
@@ -0,0 +1,12 @@
+let first = document.getElementById("first");
+// "beforebegin" - insert HTML immediately before element.
+// first.insertAdjacentHTML('beforebegin', '<div class="test">Before Begin</div>');
+// "beforeend" - insert HTML into element at the end.
+// first.insertAdjacentHTML('beforeend', '<div class="test">Before End</div>');
+// "afterbegin" - insert HTML into element at the beginning.
+// first.insertAdjacentHTML('afterbegin', '<div class="test">After Begin</div>');
+// "afterend" - insert HTML immediately after element.
+// first.insertAdjacentHTML('afterend', '<div class="test">After End</div>');
+
+// Node Removal - it is used to remove the node.
+// first.remove();
