5d8f6b0d3a2504a710e62a47cecae29ee19d0e0d
adeeb mansoor <adeebmansoor17@gmail.com>
2024-10-15 00:41:26 +0530

    new

---
+++ Ch8SetTimeout_SetIntervals.js
@@ -0,0 +1,23 @@
+// document.write("Hello World!");
+
+// setTimeout: it allows to run a function once after the given interval of time.
+// let a = setTimeout(function(){
+//     console.log("Set Timeout");
+// }, 3000);
+// console.log(a);
+
+// Set Timeout with arguments..
+// let b = setTimeout(function(a , b){
+// console.log("I am inside Set Timeout " + (a + b));
+// }, 5000, 8, 6);
+
+// clearTimeout(): it is used to cancel the execution of SetTimeout.
+// clearTimeout(b);
+
+// setInterval(): it runs the function not only once, but regularly after the given interval of time.
+// let c = setInterval(function(){
+//     console.log("I am inside setInterval");
+// }, 5000);
+
+// clearInterval(): it is used to cancel the execution of SetInterval.
+// clearInterval(c);
