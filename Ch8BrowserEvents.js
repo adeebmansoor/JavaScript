80ec78a3aeb84d17e4383147ac3b3c3016f8c544
adeeb mansoor <adeebmansoor17@gmail.com>
2024-10-15 02:31:59 +0530

    new

--- Ch8BrowserEvents.js
+++ Ch8BrowserEvents.js
@@ -1,5 +1,24 @@
-let a = document.getElementsByClassName("container")[0];
-a.onclick = () => {
-    let b = document.getElementsByClassName("container")[0];
-    b.innerHTML = "Hello World!";
+// let a = document.getElementsByClassName("container")[0];
+// a.onclick = () => {
+//     let b = document.getElementsByClassName("container")[0];
+//     b.innerHTML = "Hello World!";
+// }
+
+let btn = document.getElementById("btn");
+let x = function(e){
+    alert("Hello World1!");
 }
+btn.addEventListener('click', x);
+
+let y = function(e){
+    alert("Hello World2!");
+}
+btn.addEventListener('click', y);
+
+let a = prompt("What's your favorite number: ");
+if (a == "2"){
+    btn.removeEventListener('click', x);
+}
+
+// addEventListener() is used to assign multiple handlers to an event.
+// For removeEventListener() handler must be the same function object for this to work.
