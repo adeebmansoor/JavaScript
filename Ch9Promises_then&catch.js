635a63a746adf8989af43e72fbb5862238913630
adeeb mansoor <adeebmansoor17@gmail.com>
2024-11-11 04:22:27 +0530

    new

--- Ch9Promises_then&catch.js
+++ Ch9Promises_then&catch.js
@@ -31,23 +31,46 @@
 // });
 
 // Promise Chaining:-
-let p1 = new Promise ((resolve, reject)=>{
-    setTimeout(()=>{
-        console.log("P1 Resolved after 3 seconds.");
-        resolve(55);
-    }, 3000);
-});
-p1.then((value)=>{
+// let p1 = new Promise ((resolve, reject)=>{
+//     setTimeout(()=>{
+//         console.log("P1 Resolved after 3 seconds.");
+//         resolve(55);
+//     }, 3000);
+// });
+// p1.then((value)=>{
+//     console.log(value);
+//         var p2 = new Promise ((resolve, reject)=>{
+//         setTimeout(()=> {
+//             resolve("Promise P2");
+//         }, 3000)
+//     })
+//     return p2;
+// }).then((value) =>{
+//     console.log("We are not done yet.");
+//     return 2;
+// }).then((value) =>{
+//     console.log("Now we are actually done.");
+// });
+
+// LoadScript function using Promises:-
+const loadScript = (src) => {
+    return new Promise((resolve, reject) => {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = src;
+        document.body.appendChild(script);
+        script.onload = () => {
+            resolve("Script loaded successfully");
+        };
+        script.onerror = () => {
+            reject("Failed to load script");
+        };
+    });
+};
+
+const p1 = loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js");
+p1.then((value) => {
     console.log(value);
-        var p2 = new Promise ((resolve, reject)=>{
-        setTimeout(()=> {
-            resolve("Promise P2");
-        }, 3000)
-    })
-    return p2;
-}).then((value) =>{
-    console.log("We are not done yet.");
-    return 2;
-}).then((value) =>{
-    console.log("Now we are actually done.");
+}).catch((error) => {
+    console.error(error);
 });
