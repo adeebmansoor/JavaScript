946c290d9aca629b1356e6034bce6040f104c3a4
adeeb mansoor <adeebmansoor17@gmail.com>
2024-11-19 02:34:48 +0530

    new

---
+++ Ch9ErrorHandling_Try&Catch.js
@@ -0,0 +1,22 @@
+setTimeout(()=>{
+    console.log("Hacking Wifi....Please wait");
+}, 1000);
+
+try{
+    console.log(rahul);
+}
+catch(error){
+    console.log(error);
+}
+
+setTimeout(()=>{
+    console.log("Fetching Username and Password");
+}, 2000);
+
+setTimeout(()=>{
+    console.log("Hacking Rahul's Facebbok Id");
+}, 3000);
+
+setTimeout(()=>{
+    console.log("Username and Password of Rahul(+917002234724) is fetched");
+}, 4000);
