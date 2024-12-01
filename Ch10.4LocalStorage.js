a6c17e53e7b9c7b87fa5fea2db6e965a745f05f3
adeeb mansoor <adeebmansoor17@gmail.com>
2024-12-01 19:02:37 +0530

    new

--- Ch10.4LocalStorage.js
+++ Ch10.4LocalStorage.js
@@ -1,27 +1,33 @@
 // Some methods provided by the local storage:-
 
-let key = prompt("What is your key: ");
-let value = prompt("What is your value: ");
+// let key = prompt("What is your key: ");
+// let value = prompt("What is your value: ");
 
 // 1. setItem(key, value) - it stores key/value pair.
-localStorage.setItem(key, value);
+// localStorage.setItem(key, value);
 
-// 2. getItem(key) - get the value by key.
-console.log(`The value at ${key} is ${localStorage.getItem(key)}`);
+// // 2. getItem(key) - get the value by key.
+// console.log(`The value at ${key} is ${localStorage.getItem(key)}`);
 
-// 3. removeItem(key) - removes the key with its value.
-if (key == "red" || key == "blue"){
-    localStorage.removeItem(key);
-};
+// // 3. removeItem(key) - removes the key with its value.
+// if (key == "red" || key == "blue"){
+//     localStorage.removeItem(key);
+// };
 
-// 4. clear() - clears everything.
-if (key == 0){
-    localStorage.clear();
-};
+// // 4. clear() - clears everything.
+// if (key == 0){
+//     localStorage.clear();
+// };
 
-// 5. length - the number of stored items.
-console.log(localStorage.length);
+// // 5. length - the number of stored items.
+// console.log(localStorage.length);
 
-// 6. key(index) - get the key on a given number.
-console.log(localStorage.key(0));
-console.log(localStorage.key(2));
+// // 6. key(index) - get the key on a given number.
+// console.log(localStorage.key(0));
+// console.log(localStorage.key(2));
+
+// STORAGE EVENTS: THESE ARE EVENTS THAT TRIGGERS WHEN THERE IS A CHANGE IN THE DATA OF LOCALSTORAGE & SESSIONSTORAGE
+// window.onstorage = (e) => {
+//     alert("Changed");
+//     console.log(e);
+// }
